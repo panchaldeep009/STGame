fc10e6ec832f94763662c7feaff7fab37417d5f9
panchaldeep009 <panchaldeep009@gmail.com>
2018-05-24 21:36:58 -0400

    Change player Control and Bullet travel, Graphical Player added

--- js/main.js
+++ js/main.js
@@ -42,13 +42,17 @@ var player = [];
 // initilize First Players
 player[1] = [];
 // Set Player[N][0] = size
-player[1][0] = 50;
+player[1][0] = 120;
+// Set Player[N][1] = canvas position X
+player[1][1] = (canvasSize/2);
+// Set Player[N][2] = canvas position Y
+player[1][2] = (canvasSize/2);
 // Set Player[N][1] = position X
-player[1][1] = (canvasSize/2)-(player[1][0]/2);
+player[1][5] = -(player[1][0]/2);
 // Set Player[N][2] = position Y
-player[1][2] = (canvasSize/2)-(player[1][0]/2);
+player[1][6] = -(player[1][0]/2);
 // Set Player[N][3] = style
-player[1][3] = "#FF0000";
+player[1][3] = 0;
 // Set Player[N][4] = Rotation
 player[1][4] = 0;
 //Veriable to Bullets
@@ -64,30 +68,40 @@ document.onmousemove = function(e) {
     mouseX = e.pageX - offset.left;
     mouseY = e.pageY - offset.top;
 }
+var movePlayer = true;
 // on keypress
 document.onkeydown = function(e) {
+    if(movePlayer){
     e = e || window.event;
-    // if this key is Up key
-    if (e.keyCode == '38') {
-        // move player to up
-        player[1][2] -= 12;
+    // if this key is Space key
+    if (e.keyCode == '32') {
+        // Save current Degree
+        currentDeg = (player[1][4]* Math.PI) / 180;
+        // Prevent player to take next move
+        movePlayer = false;
+        setTimeout(function(){ 
+            // Change Player Style
+            player[1][3] = 100;   
+            // Change Player canvas in diagonal line of degree
+            player[1][2] += (Math.sin(currentDeg))*20;
+            player[1][1] += (Math.cos(currentDeg))*20;
+            setTimeout(function(){
+                // Change Player Style
+                player[1][3] = 200;
+                // Change Player canvas in diagonal line of degree
+                player[1][2] += (Math.sin(currentDeg))*20;
+                player[1][1] += (Math.cos(currentDeg))*20;
+                setTimeout(function(){
+                    // Change Player Style
+                    player[1][3] = 0;
+                    // Change Player canvas in diagonal line of degree
+                    player[1][2] += (Math.sin(currentDeg))*20;
+                    player[1][1] += (Math.cos(currentDeg))*20; 
+                    movePlayer = true;
+                },150);
+            },150); 
+        },150);
     }
-    // if this key is Left key
-    else if (e.keyCode == '37') {
-        // move player to left
-        player[1][1] -= 12;
-    }
-    // if this key is Right key
-    else if (e.keyCode == '39') {
-        // move player to right
-        player[1][1] += 12;
-    }
-    // if this key is Down key
-    else if (e.keyCode == '40') {
-        // move player to down
-        player[1][2] += 12;
-    }
-
     if(player[1][1] <= 0){
         // To stop player at minimum X
         player[1][1] = 0;
@@ -105,10 +119,11 @@ document.onkeydown = function(e) {
         player[1][2] = canvasSize-player[1][0];
     }
 }
+}
 // on click add new bullet to arrary for fire
 document.onclick = function(e){
     //bullet width
-    bw = 40;
+    bw = 16;
     //bullet height
     bh = 8;
     //New Bullet number
@@ -117,70 +132,75 @@ document.onclick = function(e){
     bullet[i] = [];
     //bullet[N][0] is Rotation
     bullet[i][0] = player[1][4]; 
-    //bullet[N][1] is X postion
-    bullet[i][1] = player[1][0]/4; 
-    //bullet[N][2] is Y postion
-    bullet[i][2] = (player[1][0]/2)-bh*2;
+    //bullet[N][1] is canvas translation X
+    bullet[i][1] = player[1][1]+player[1][5]+(player[1][0]/2);
+    //bullet[N][2] is canvas translation Y
+    bullet[i][2] = player[1][2]+player[1][6]+(player[1][0]/2);
     //bullet[N][3] is width
     bullet[i][3] = bw;
     //bullet[N][4] is Height
     bullet[i][4] = bh;
     //bullet[N][5] is Style
     bullet[i][5] = "#FFFF00";
-    //bullet[N][6] is canvas translation X
-    bullet[i][6] = player[1][1]+(player[1][0]/2);
-    //bullet[N][6] is canvas translation Y
-    bullet[i][7] = player[1][2]+(player[1][0]/2);
+    //bullet[N][6] is X postion of bullet inside of canvas
+    bullet[i][6] = 50; 
+    //bullet[N][7] is Y postion of bullet inside of canvas
+    bullet[i][7] = 20;
 }
 
 // Refresh Game canvas at 60 fps
 var gamePlay = setInterval(function(){ 
     // Clear Canvas
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(0, 0, canvasSize, canvasSize);
+    ctx.fillRect(0, 0, canvasSize, canvasSize);              
 
     // Put player in canvas
-    // Get player Style
-    ctx.fillStyle = player[1][3];
     // Get radians by Anti Tan of X any Y side from player origin
     radians = Math.atan2(mouseX-player[1][1], mouseY-player[1][2]);
-    // Get degree from radians
-    player[1][4] = (radians * (180 / Math.PI) * -1) + 90;
+    // Change degree of player if movement allowed
+    if(movePlayer){
+        player[1][4] = Math.round((radians * (180 / Math.PI) * -1)+90);
+    }
     // translate ctx to player's XY cordinates
-    ctx.translate(player[1][1]+player[1][0]/2,player[1][2]+player[1][0]/2);
+    ctx.translate(player[1][1],player[1][2]);
     // Rotate ctx
     ctx.rotate((player[1][4]* Math.PI) / 180);
-    // fill the player at position offset by half its size
-    ctx.fillRect(-player[1][0]/2, -player[1][0]/2, player[1][0], player[1][0]);
+    // Player Image
+    playerImg = el("#player");
+    // Draw Player Image on canvas with offset pixels from player[1][3] and Size from player[1][0]
+    ctx.drawImage(playerImg, player[1][3], 0, 100, 100, player[1][5], player[1][6], player[1][0], player[1][0]);
     // Reset Rotate
     ctx.rotate((-player[1][4] * Math.PI) / 180);
     // Reset Traslate
-    ctx.translate(-player[1][1]-player[1][0]/2,-player[1][2]-player[1][0]/2);
+    ctx.translate(-player[1][1],-player[1][2]);
 
     // Print all bullets thats stored in arrey 
     for(i=0;i<bullet.length;i++){
         // fill Bullet Style
-        ctx.fillStyle = bullet[i][5];
+        ctx.fillStyle = bullet[i][5]; 
         // Translate cansvas to bullet position
-        ctx.translate(bullet[i][6],bullet[i][7]);
+        ctx.translate(bullet[i][1],bullet[i][2]);
         // rotate canvas 
         ctx.rotate((bullet[i][0] * Math.PI) / 180);
         // Fill bullet in canvas
-        ctx.fillRect(bullet[i][1], bullet[i][2], bullet[i][3], bullet[i][4]);
+        ctx.fillRect(bullet[i][6], bullet[i][7], bullet[i][3], bullet[i][4]);
         // reset rotate canvas 
         ctx.rotate((-bullet[i][0] * Math.PI) / 180);
         // reset translate canvas 
-        ctx.translate(-bullet[i][6],-bullet[i][7]);
-        // bullet travel by incrementing it's X position
-        bullet[i][1]=bullet[i][1]+10;
+        ctx.translate(-bullet[i][1],-bullet[i][2]);
+        // get current bullet degree
+        currentBulletDeg = (bullet[i][0]* Math.PI) / 180;
+        // Travel bullet X and Y axies of canvas on dignal line of dgree
+        bullet[i][1] += (Math.cos(currentBulletDeg))*10;
+        bullet[i][2] += (Math.sin(currentBulletDeg))*10;
         //delete bullet when it get out of canvas
-        if(bullet[i][1] > canvasSize){
+        if(bullet[i][1] > canvasSize || bullet[i][2] > canvasSize ){
             bullet.splice(i,1);
         }
     }
     
     // print traget Circle
-    tSize = 30;
+    tSize = 50;
     ctx.beginPath();
     ctx.arc(mouseX, mouseY, tSize/2, 0, 2 * Math.PI);
     ctx.stroke();
